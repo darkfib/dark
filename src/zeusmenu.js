86174434d97d2e357fd85ce9d6de88aa4ecaa639
darkfib <rv7461052@Gmail.com>
2021-02-14 12:02:24 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,40 @@
+const Hokgmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Hokg:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *Hokg*
+  DUVIDAS? 👇
+  WA.me/5533998499824
+╚════════════════════`
+}
+
+exports.Hokgmenu = Hokgmenu
+
+
+
+
+
+
+
+
